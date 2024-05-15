3d9785b06b42ae9ee59465d426436a06c8090ad1
ZePoulet78 <vdonnesh@gmail.com>
2024-05-15 12:42:17 +0200

    add index activities for volunteers

---
+++ src/pages/Public1/ProfilePages/Volunteer/IndexActivities.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { activityService } from '@/_service/activity.service';
+import { Link } from 'react-router-dom';
+
+
+const IndexActivitites = () => {
+  const [activity, setActivity] = useState([]);
+
+
+  useEffect(() => {
+      
+    activityService.getAllActivity()
+        .then(res => {
+                setActivity(res.data.activity)
+            })
+        .catch(console.log("error"))
+        
+    }, [])
+
+    const delAct = (actId) => {
+    activityService.deleteUser(actId)
+        .then(res => {
+        setActivity((current) => current.filter(activity => activity.id !== actId));
+        })
+        .catch(err => console.log(err));
+    };
+
+
+
+  if (!Array.isArray(activity) || activity.length === 0) {
+    return (
+      <div>
+          <div>Aucune donnée à afficher.</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mt-5">
+      <h2>Gestion des Activity</h2>
+      <Link to ="/admin/activity/add/"><button className="btn btn-primary btn-sm">Ajouter</button></Link>
+      <table className="table border rounded table-rounded">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Prénom</th>
+            <th scope="col">Email</th>
+            <th scope="col">Téléphone</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activity.map(activity => (
+            <tr key={activity.id}>
+              <th scope="row">{activity.id}</th>
+              <td>{activity.heure_debut}</td>
+              <td>{activity.heure_fin}</td>
+              <td>{activity.date}</td>
+              <td>{activity.type}</td>
+              <td>{activity.description}</td>
+              <td>
+                <button className="bi bi-eye"><img src="/eyes.svg"></img></button>
+                <Link to ={`/admin/activity/edit/${activity.id}`}><button className="btn btn-primary btn-sm">Modifier</button></Link>
+                <button
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-3 md:px-4 rounded"
+                onClick={() => delAct(activity.id)}
+                >
+                    Supprimer
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+export default IndexActivitites;
